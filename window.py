6f90bcf225b13542a6dd5a1071104fb760f5d766
Leslie Poolman <leslie.john.poolman01@gmail.com>
2024-02-29 10:38:44 -0500

    Corrected my understanding of the canvas

--- window.py
+++ window.py
@@ -2,14 +2,12 @@ from tkinter import Tk, BOTH, Canvas
 
 class Window:
     def __init__(self, width, height):
-        self.width = width
-        self.height = height
         self.__root_widget = Tk()
-        self.__canvas = Canvas()
+        self.__root_widget.title('Funtastic Maze')
+        self.__canvas = Canvas(self.__root_widget, bg="grey", height=height, width=width)
         self.__window_running = False
         self.__canvas.pack()
         self.__root_widget.protocol("WM_DELETE_WINDOW", self.close)
-        self.__root_widget.title('Funtastic Maze')
 
     def redraw(self):
         self.__root_widget.update_idletasks()
@@ -19,6 +17,7 @@ class Window:
         self.__window_running = True
         while self.__window_running:
             self.redraw()
+        print("Maze Vanished!")
     
     def close(self):
         self.__window_running = False
