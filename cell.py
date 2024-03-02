b9a6cde89768bff0529ad350216e238ca03ae1eb
Leslie Poolman <leslie.john.poolman01@gmail.com>
2024-03-02 16:22:28 -0500

    Added draw move for movement between cells

---
+++ cell.py
@@ -0,0 +1,72 @@
+from graphics import Line, Point
+
+class Cell:
+    def __init__(self, win, l_wall=True, r_wall=True, t_wall=True, b_wall=True):
+        self.has_left_wall = l_wall
+        self.has_right_wall = r_wall
+        self.has_top_wall = t_wall
+        self.has_bottom_wall = b_wall
+        self.__win = win
+        self.__x1 = None
+        self.__x2 = None
+        self.__y1 = None
+        self.__y2 = None
+
+    def set_top_left(self, point):
+        self.__x1 = point.x
+        self.__y1 = point.y
+
+    def set_btm_right(self, point):
+        self.__x2 = point.x
+        self.__y2 = point.y
+
+    def draw(self, top_left, btm_right):
+        if self.__win is None:
+            return
+        self.set_btm_right(btm_right)
+        self.set_top_left(top_left)
+        if self.has_left_wall:
+            line = Line(top_left, Point(self.__x1, self.__y2))
+            self.__win.draw_line(line, "pink")
+        if self.has_right_wall:
+            line = Line(Point(self.__x2, self.__y1), btm_right)
+            self.__win.draw_line(line, "pink")
+        if self.has_top_wall:
+            line = Line(top_left, Point(self.__x2, self.__y1))
+            self.__win.draw_line(line, "pink")            
+        if self.has_bottom_wall:
+            line = Line(Point(self.__x1, self.__y2), btm_right)
+            self.__win.draw_line(line, "pink")
+    
+    def draw_move(self, to_cell, undo=False, colour="green"):
+        if self.__win is None:
+            return
+        if undo:
+            colour = "orange"
+
+        s_mid_y = (self.__y1 + self.__y2) / 2
+        s_mid_x = (self.__x1 + self.__x2) / 2
+
+        to_mid_y = (to_cell.__y1 + to_cell.__y2) / 2
+        to_mid_x = (to_cell.__x1 + to_cell.__x2) / 2
+
+        if self.__x1 > to_cell.__x1:
+            from_center_to_left_edge = Line(Point(self.__x1, s_mid_y), Point(s_mid_x, s_mid_y))
+            from_left_edge_to_center = Line(Point(to_mid_x, to_mid_y), Point(to_cell.__x2, to_mid_y))
+            self.__win.draw_line(from_center_to_left_edge, colour)
+            self.__win.draw_line(from_left_edge_to_center, colour)
+        elif self.__x1 < to_cell.__x1:
+            from_center_to_right_edge = Line(Point(s_mid_x, s_mid_y), Point(self.__x2, s_mid_y))
+            from_right_edge_to_center = Line(Point(to_cell.__x1, to_mid_y), Point(to_mid_x, to_mid_y))
+            self.__win.draw_line(from_center_to_right_edge, colour)
+            self.__win.draw_line(from_right_edge_to_center, colour)
+        elif self.__y1 > to_cell.__y1:
+            from_center_to_top_edge = Line(Point(s_mid_x, s_mid_y), Point(s_mid_x, self.__y1))
+            from_top_edge_to_center = Line(Point(to_mid_x, to_cell.__y2), Point(to_mid_x, to_mid_y))
+            self.__win.draw_line(from_center_to_top_edge, colour)
+            self.__win.draw_line(from_top_edge_to_center, colour)
+        elif self.__y1 < to_cell.__y1:
+            from_center_to_bottom_edge = Line(Point(s_mid_x, s_mid_y), Point(s_mid_x, self.__y2))
+            from_bottom_edge_to_center = Line(Point(to_mid_x, to_mid_y), Point(to_mid_x, to_cell.__y1))
+            self.__win.draw_line(from_center_to_bottom_edge, colour)
+            self.__win.draw_line(from_bottom_edge_to_center, colour)
--- main.py
+++ main.py
@@ -1,12 +1,32 @@
 from graphics import *
-import random
+from cell import *
 
 def main():
     win = Window(800, 600)
-    for i in range(500):
-        cell = Cell(random.choice([True, False]), random.choice([True, False]), random.choice([True, False]), random.choice([True, False]))
-        cell.set_window(win)
-        cell.draw(Point(i*2, i*3), Point(i*4, i*6))
+    cell1 = Cell(win, r_wall=False)
+    cell1.draw(Point(10, 10), Point(100, 100))
+    cell2 = Cell(win, l_wall=False, b_wall=False)
+    cell2.draw(Point(100, 10), Point(200, 100))
+    cell1.draw_move(cell2)
+
+    cell3 = Cell(win, r_wall=False, t_wall=False)
+    cell3.draw(Point(100, 100), Point(200, 200))
+    cell2.draw_move(cell3)
+
+    cell4 = Cell(win, l_wall=False, t_wall=False)
+    cell4.draw(Point(200, 100), Point(300, 200))
+    cell3.draw_move(cell4)
+
+    cell5 = Cell(win, b_wall=False, r_wall=False)
+    cell5.draw(Point(200, 10), Point(300, 100))
+    cell4.draw_move(cell5, undo=True)
+
+    cell6 = Cell(win, r_wall=False)
+    cell6.draw(Point(200, 300), Point(300, 400))
+    cell7 = Cell(win, l_wall=False)
+    cell7.draw(Point(300, 300), Point(400, 400))
+    cell6.draw_move(cell7, undo=True)
+
     win.wait_for_close()
 
 main()
