ba5b838c91489f41ce2eaade1899c070fbd59105
Leslie Poolman <leslie.john.poolman01@gmail.com>
2024-03-03 10:28:13 -0500

    add entrance and exit to Maze

--- maze.py
+++ maze.py
@@ -12,6 +12,7 @@ class Maze:
         self.__cell_size_y = cell_size_y
         self.__win = win
         self.__create_cells()
+        self.__break_entrance_and_exit()
 
     def __create_cells(self):
         for col in range(self.__num_cols):
@@ -36,3 +37,9 @@ class Maze:
             return
         self.__win.redraw()
         time.sleep(0.05)
+
+    def __break_entrance_and_exit(self):
+        self._cells[0][0].has_top_wall = False
+        self._cells[-1][-1].has_bottom_wall = False
+        self.__draw_cell(0, 0)
+        self.__draw_cell(self.__num_cols - 1, self.__num_rows - 1)
--- test.py
+++ test.py
@@ -17,7 +17,7 @@ class Tests(unittest.TestCase):
 
     def test_maze_create_cells2(self):
         num_cols = 24
-        num_rows = 8
+        num_rows = 24
         m1 = Maze(0, 0, num_rows, num_cols, 10, 10)
         self.assertEqual(
             len(m1._cells),
@@ -27,6 +27,13 @@ class Tests(unittest.TestCase):
             len(m1._cells[0]),
             num_rows,
         )
+
+    def test_maze_creates_entrance_and_exit(self):
+        num_cols = 12
+        num_rows = 10
+        m1 = Maze(0, 0, num_rows, num_cols, 10, 10)
+        self.assertFalse(m1._cells[0][0].has_top_wall)
+        self.assertFalse(m1._cells[num_cols - 1][num_rows - 1].has_bottom_wall)
 
 if __name__ == "__main__":
     unittest.main()
